5715bc90bdb555c7090ea7764657bc0cb5cee59b
Feromon <65503900+Feromon32@users.noreply.github.com>
2023-06-20 18:19:27 +0300

    Update printjiggle.py

--- printjiggle.py
+++ printjiggle.py
@@ -1,61 +1,63 @@
 import bpy
+import bpy.ops
+
 context = bpy.context
 obj = context.object
 
 namelist = [
-"ValveBiped.Bip01_Pelvis",
-"ValveBiped.Bip01_L_Thigh",
-"ValveBiped.Bip01_L_Calf",
-"ValveBiped.Bip01_L_Foot",
-"ValveBiped.Bip01_L_Toe0",
-"ValveBiped.Bip01_R_Thigh",
-"ValveBiped.Bip01_R_Calf",
-"ValveBiped.Bip01_R_Foot",
-"ValveBiped.Bip01_R_Toe0",
-"ValveBiped.Bip01_Spine",
-"ValveBiped.Bip01_Spine1",
-"ValveBiped.Bip01_Spine2",
-"ValveBiped.Bip01_Spine4",
-"ValveBiped.Bip01_Neck1",
-"ValveBiped.Bip01_Head1",
-"ValveBiped.Bip01_L_Clavicle",
-"ValveBiped.Bip01_L_UpperArm",
-"ValveBiped.Bip01_L_Forearm",
-"ValveBiped.Bip01_L_Hand",
-"ValveBiped.Bip01_L_Finger0",
-"ValveBiped.Bip01_L_Finger01",
-"ValveBiped.Bip01_L_Finger02",
-"ValveBiped.Bip01_L_Finger1",
-"ValveBiped.Bip01_L_Finger11",
-"ValveBiped.Bip01_L_Finger12",
-"ValveBiped.Bip01_L_Finger2",
-"ValveBiped.Bip01_L_Finger21",
-"ValveBiped.Bip01_L_Finger22",
-"ValveBiped.Bip01_L_Finger3",
-"ValveBiped.Bip01_L_Finger31",
-"ValveBiped.Bip01_L_Finger32",
-"ValveBiped.Bip01_L_Finger4",
-"ValveBiped.Bip01_L_Finger41",
-"ValveBiped.Bip01_L_Finger42",
-"ValveBiped.Bip01_R_Clavicle",
-"ValveBiped.Bip01_R_UpperArm",
-"ValveBiped.Bip01_R_Forearm",
-"ValveBiped.Bip01_R_Hand",
-"ValveBiped.Bip01_R_Finger0",
-"ValveBiped.Bip01_R_Finger01",
-"ValveBiped.Bip01_R_Finger02",
-"ValveBiped.Bip01_R_Finger1",
-"ValveBiped.Bip01_R_Finger11",
-"ValveBiped.Bip01_R_Finger12",
-"ValveBiped.Bip01_R_Finger2",
-"ValveBiped.Bip01_R_Finger21",
-"ValveBiped.Bip01_R_Finger22",
-"ValveBiped.Bip01_R_Finger3",
-"ValveBiped.Bip01_R_Finger31",
-"ValveBiped.Bip01_R_Finger32",
-"ValveBiped.Bip01_R_Finger4",
-"ValveBiped.Bip01_R_Finger41",
-"ValveBiped.Bip01_R_Finger42"
+    "ValveBiped.Bip01_Pelvis",
+    "ValveBiped.Bip01_L_Thigh",
+    "ValveBiped.Bip01_L_Calf",
+    "ValveBiped.Bip01_L_Foot",
+    "ValveBiped.Bip01_L_Toe0",
+    "ValveBiped.Bip01_R_Thigh",
+    "ValveBiped.Bip01_R_Calf",
+    "ValveBiped.Bip01_R_Foot",
+    "ValveBiped.Bip01_R_Toe0",
+    "ValveBiped.Bip01_Spine",
+    "ValveBiped.Bip01_Spine1",
+    "ValveBiped.Bip01_Spine2",
+    "ValveBiped.Bip01_Spine4",
+    "ValveBiped.Bip01_Neck1",
+    "ValveBiped.Bip01_Head1",
+    "ValveBiped.Bip01_L_Clavicle",
+    "ValveBiped.Bip01_L_UpperArm",
+    "ValveBiped.Bip01_L_Forearm",
+    "ValveBiped.Bip01_L_Hand",
+    "ValveBiped.Bip01_L_Finger0",
+    "ValveBiped.Bip01_L_Finger01",
+    "ValveBiped.Bip01_L_Finger02",
+    "ValveBiped.Bip01_L_Finger1",
+    "ValveBiped.Bip01_L_Finger11",
+    "ValveBiped.Bip01_L_Finger12",
+    "ValveBiped.Bip01_L_Finger2",
+    "ValveBiped.Bip01_L_Finger21",
+    "ValveBiped.Bip01_L_Finger22",
+    "ValveBiped.Bip01_L_Finger3",
+    "ValveBiped.Bip01_L_Finger31",
+    "ValveBiped.Bip01_L_Finger32",
+    "ValveBiped.Bip01_L_Finger4",
+    "ValveBiped.Bip01_L_Finger41",
+    "ValveBiped.Bip01_L_Finger42",
+    "ValveBiped.Bip01_R_Clavicle",
+    "ValveBiped.Bip01_R_UpperArm",
+    "ValveBiped.Bip01_R_Forearm",
+    "ValveBiped.Bip01_R_Hand",
+    "ValveBiped.Bip01_R_Finger0",
+    "ValveBiped.Bip01_R_Finger01",
+    "ValveBiped.Bip01_R_Finger02",
+    "ValveBiped.Bip01_R_Finger1",
+    "ValveBiped.Bip01_R_Finger11",
+    "ValveBiped.Bip01_R_Finger12",
+    "ValveBiped.Bip01_R_Finger2",
+    "ValveBiped.Bip01_R_Finger21",
+    "ValveBiped.Bip01_R_Finger22",
+    "ValveBiped.Bip01_R_Finger3",
+    "ValveBiped.Bip01_R_Finger31",
+    "ValveBiped.Bip01_R_Finger32",
+    "ValveBiped.Bip01_R_Finger4",
+    "ValveBiped.Bip01_R_Finger41",
+    "ValveBiped.Bip01_R_Finger42"
 ]
 
 ob = bpy.context.object
@@ -65,27 +67,33 @@ if ob.type == 'ARMATURE':
 
     for bone in armature.bones:
         armaturebones.append(bone.name)
-        
 
 for item in namelist:
     if item in armaturebones:
         armaturebones.remove(item)
 
+output_strings = []
 
 for v in armaturebones:
-    print("$jigglebone " + '"' + v + '"')
-    print("{")
-    print("    is_flexible")
-    print("    {")
-    print("        length 10")
-    print("        tip_mass 200")
-    print("        pitch_stiffness 75")
-    print("        pitch_damping 6")
-    print("        yaw_stiffness 75")
-    print("        yaw_damping 6")
-    print("        along_stiffness 100")
-    print("        along_damping 0")
-    print("        angle_constraint 10")
-    print("    }")
-    print("}")
-        
+    output_strings.append("$jigglebone " + '"' + v + '"')
+    output_strings.append("{")
+    output_strings.append("    is_flexible")
+    output_strings.append("    {")
+    output_strings.append("        length 10")
+    output_strings.append("        tip_mass 10")
+    output_strings.append("        pitch_stiffness 40")
+    output_strings.append("        pitch_constraint -70 0")
+    output_strings.append("        pitch_damping 6")
+    output_strings.append("        yaw_stiffness 75")
+    output_strings.append("        yaw_damping 6")
+    output_strings.append("        along_stiffness 100")
+    output_strings.append("        along_damping 0")
+    output_strings.append("        angle_constraint 10")
+    output_strings.append("    }")
+    output_strings.append("}")
+
+output_text = '\n'.join(output_strings)
+
+bpy.context.window_manager.clipboard = output_text
+
+print("Done, press on your qc 'Ctrl + v'")
