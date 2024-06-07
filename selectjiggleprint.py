21674ded953f4ee042a72c513a588334d9130a5b
Feromon32 <65503900+Feromon32@users.noreply.github.com>
2024-06-07 04:24:20 +0300

    Update selectjiggleprint.py

--- selectjiggleprint.py
+++ selectjiggleprint.py
@@ -4,25 +4,29 @@ obj = bpy.context.object
 
 selected_bones = [bone.name for bone in obj.data.bones if bone.select]
 
-output_text = '\n'.join([
-    f'$jigglebone "{bone}"\n'
-    '{\n'
-    '    is_flexible\n'
-    '    {\n'
-    '        length 10\n'
-    '        tip_mass 10\n'
-    '        pitch_stiffness 40\n'
-    '        pitch_constraint -70 0\n'
-    '        pitch_damping 6\n'
-    '        yaw_stiffness 75\n'
-    '        yaw_damping 6\n'
-    '        along_stiffness 100\n'
-    '        along_damping 0\n'
-    '        angle_constraint 10\n'
-    '    }\n'
-    '}'
-    for bone in selected_bones
-])
+# just change this settings, if need that
+jigglebone_template = '''
+$jigglebone "{bone}"
+{{
+    is_flexible
+    {{
+        length 10
+        tip_mass 10
+        pitch_stiffness 40
+        pitch_constraint -70 0
+        pitch_damping 6
+        yaw_stiffness 75
+        yaw_damping 6
+        along_stiffness 100
+        along_damping 0
+        angle_constraint 10
+    }}
+}}
+'''
+
+output_text = '\n'.join([jigglebone_template.format(bone=bone) for bone in selected_bones])
+
 
 bpy.context.window_manager.clipboard = output_text
+
 print("Done, press 'Ctrl + V' in your qc file.")
